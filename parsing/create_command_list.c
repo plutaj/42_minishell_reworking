88e8f9cb97415f8a720a1c339a2d4f2632552f78
plutaj <jozefp048@gmail.com>
2025-06-07 16:17:09 +0200

    working quotes

--- parsing/create_command_list.c
+++ parsing/create_command_list.c
@@ -6,7 +6,7 @@
 /*   By: jozefpluta <jozefpluta@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:08:56 by jpluta            #+#    #+#             */
-/*   Updated: 2025/06/06 19:12:27 by jozefpluta       ###   ########.fr       */
+/*   Updated: 2025/06/07 15:44:58 by jozefpluta       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ void	create_command_list(t_data *data)
 		new_cmd->data = data;
 		new_cmd->next = NULL;
 		new_cmd = split_args_and_redirs(new_cmd, s[i]);
+		print_linked_list(data->cmd_list); // there
 		find_variables(new_cmd);
 		if (!data->cmd_list)
 			data->cmd_list = new_cmd;
@@ -63,23 +64,41 @@ t_command *split_args_and_redirs(t_command *new_cmd, char *s)
 			continue;
 		}
 
-		// Start or end a quote block
-		if ((s[i] == '\'' || s[i] == '\"'))
+		// Handle single quote block — copy everything literally
+		if (s[i] == '\'' && !quote)
 		{
-			if (!quote)
-				quote = s[i];
-			else if (quote == s[i])
-				quote = 0;
-			else
-				buffer[buf_i++] = s[i]; // mismatched quote, keep it
-			i++;
+			quote = '\'';
+			i++; // skip opening quote
+			while (s[i] && s[i] != '\'')
+				buffer[buf_i++] = s[i++];
+			if (s[i] == '\'')
+				i++; // skip closing quote
+			quote = 0;
 			continue;
 		}
 
-		// Add char to buffer
+		// Handle double quote block
+		if (s[i] == '\"')
+		{
+			if (!quote)
+			{
+				quote = '\"';
+				i++;
+				continue;
+			}
+			else if (quote == '\"')
+			{
+				quote = 0;
+				i++;
+				continue;
+			}
+		}
+
+		// Add character to buffer
 		buffer[buf_i++] = s[i++];
 	}
 
+	// If anything remains in buffer, save it as the last argument
 	if (buf_i > 0)
 	{
 		buffer[buf_i] = '\0';
@@ -92,61 +111,55 @@ t_command *split_args_and_redirs(t_command *new_cmd, char *s)
 }
 
 
-// t_command	*split_args_and_redirs(t_command *new_cmd, char *s)
+// t_command *split_args_and_redirs(t_command *new_cmd, char *s)
 // {
-// 	char	**new_s;
-// 	int		i;
-// 	int		arg_i;
-// 	char	quote;
-// 	char	*temp;
-// 	char	*arg;
+// 	char **args = calloc(256, sizeof(char *));
+// 	int i = 0, j = 0;
+// 	char quote = 0;
+// 	char buffer[1024];
+// 	int buf_i = 0;
 
-	// new_s = ft_split(s, ' ');
-	// i = 0;
-	// arg_i = 0;
-	// quote = 0;
-	// new_cmd->args = (char **)calloc(254, sizeof(char *));
-	// while (new_s[i])
-	// {
-	// 	if (starts_with_quote(new_s[i]))
-	// 	{
-	// 		quote = new_s[i][0];
-	// 		arg = ft_strdup(new_s[i]);
-			// i++;
-
-			// while (new_s[i] && !ends_with_quote(new_s[i], quote))
-			// {
-			// 	temp = ft_strjoin(arg, " ");
-			// 	free(arg);
-			// 	arg = temp;
-
-			// 	temp = ft_strjoin(arg, new_s[i]);
-			// 	free(arg);
-			// 	arg = temp;
-			// 	i++;
-			// }
-			// if (new_s[i] && ends_with_quote(new_s[i], quote))
-			// {
-			// 	temp = ft_strjoin(arg, " ");
-			// 	free(arg);
-			// 	arg = temp;
-
-			// 	temp = ft_strjoin(arg, new_s[i]);
-// 				free(arg);
-// 				arg = temp;
-// 				i++;
-// 			}
-// 			new_cmd->args[arg_i++] = arg;
-// 		}
-// 		else
+// 	while (s[i])
+// 	{
+// 		// Skip spaces outside quotes
+// 		if (!quote && s[i] == ' ')
 // 		{
-// 			new_cmd->args[arg_i++] = ft_strdup(new_s[i]);
+// 			if (buf_i > 0)
+// 			{
+// 				buffer[buf_i] = '\0';
+// 				args[j++] = ft_strdup(buffer);
+// 				buf_i = 0;
+// 			}
 // 			i++;
+// 			continue;
 // 		}
+
+// 		// Start or end a quote block
+// 		if ((s[i] == '\'' || s[i] == '\"'))
+// 		{
+// 			if (!quote)
+// 				quote = s[i];
+// 			else if (quote == s[i])
+// 				quote = 0;
+// 			else
+// 				buffer[buf_i++] = s[i]; // mismatched quote, keep it
+// 			i++;
+// 			continue;
+// 		}
+
+// 		// Add char to buffer
+// 		buffer[buf_i++] = s[i++];
 // 	}
-// 	new_cmd->args[arg_i] = NULL;
-// 	free_2d_array(new_s);
-// 	return (new_cmd);
+
+// 	if (buf_i > 0)
+// 	{
+// 		buffer[buf_i] = '\0';
+// 		args[j++] = ft_strdup(buffer);
+// 	}
+
+// 	args[j] = NULL;
+// 	new_cmd->args = args;
+// 	return new_cmd;
 // }
 
 int starts_with_quote(const char *s)
