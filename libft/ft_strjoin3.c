670a9dbe9e32c329685b70169216f60c938a738c
Jozef Pluta <jpluta@c1r6s4.42prague.com>
2025-06-29 15:11:57 +0200

    working on normes from libft

--- libft/ft_strjoin3.c
+++ libft/ft_strjoin3.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin3.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jozefpluta <jozefpluta@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jpluta <jpluta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 16:48:07 by jozefpluta        #+#    #+#             */
-/*   Updated: 2025/05/18 16:48:55 by jozefpluta       ###   ########.fr       */
+/*   Updated: 2025/06/29 14:53:38 by jpluta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strjoin3(const char *s1, const char *s2, const char *s3)
+char	*ft_strjoin3(const char *s1, const char *s2, const char *s3)
 {
-	char *tmp;
-	char *res;
+	char	*tmp;
+	char	*res;
 
-    tmp = ft_strjoin(s1, s2);
-    res = ft_strjoin(tmp, s3);
+	tmp = ft_strjoin(s1, s2);
+	res = ft_strjoin(tmp, s3);
 	free(tmp);
-	return res;
+	return (res);
 }
